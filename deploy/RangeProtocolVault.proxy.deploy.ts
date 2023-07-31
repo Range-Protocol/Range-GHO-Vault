99a69131bd872403bf7d40b5db06be8e73e94c7f
0xbateman <0xbateman@rangeprotocol.com>
2023-07-31 15:27:31 +0800

    update proxy implementation for USDC pair with GHO

--- deploy/RangeProtocolVault.proxy.deploy.ts
+++ deploy/RangeProtocolVault.proxy.deploy.ts
@@ -8,9 +8,9 @@ async function main() {
   const managerAddress = "0x84b43ce5fB1FAF013181FEA96ffA4af6179e396a"; // To be updated.
   const rangeProtocolFactoryAddress = ""; // To be updated.
   const vaultImplAddress = ""; // to be updated.
-  const token0 = ""; // to be updated.
-  const token1 = ""; // to be updated.
-  const fee = 0; // To be updated.
+  const token0 = "0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f"; // to be updated.
+  const token1 = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"; // to be updated.
+  const fee = 3000; // To be updated.
   const name = ""; // To be updated.
   const symbol = ""; // To be updated.
 
@@ -27,7 +27,7 @@ async function main() {
     poolAddressesProvider: "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e"
   });
 
-  const tx = await factory.createVault(token0, token1, fee, vaultImplAddress, data);
+  const tx = await factory.createVault(token1, fee, vaultImplAddress, data);
   const txReceipt = await tx.wait();
   const [
     {
